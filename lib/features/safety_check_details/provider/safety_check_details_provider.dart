64734c978162de6f29d122f7d9cfd48a5cc34dcb
JabbarKakar <jabbarkakar22@gmail.com>
2025-07-06 13:16:58 +0500

    By Jabbar, Safety Check Details api minor error removed

--- lib/features/safety_check_details/provider/safety_check_details_provider.dart
+++ lib/features/safety_check_details/provider/safety_check_details_provider.dart
@@ -20,12 +20,6 @@ class SafetyCheckDetailsProvider with ChangeNotifier {
   bool get isLoading => _isLoading;
   String? get errorMessage => _errorMessage;
 
-  // final Dio _dio = Dio(BaseOptions(
-  //   baseUrl: 'https://api.amnak.app',
-  //   connectTimeout: const Duration(seconds: 10),
-  //   receiveTimeout: const Duration(seconds: 10),
-  // ));
-
   Future<void> fetchSafetyCheckDetails({required String id}) async {
     if (await networkInfo.isConnected) {
       try {
@@ -59,26 +53,4 @@ class SafetyCheckDetailsProvider with ChangeNotifier {
       notifyListeners();
     }
   }
-
-  // Future<void> fetchSafetyCheckDetails(String id) async {
-  //   _isLoading = true;
-  //   _errorMessage = null;
-  //   notifyListeners();
-  //
-  //   try {
-  //     final response = await _dio.get('/safety_check/$id');
-  //     if (response.statusCode == 200) {
-  //       _safetyCheckDetails = SafetyCheckDetailsModel.fromJson(response.data);
-  //     } else {
-  //       debugPrint('Failed to load data: ${response.statusCode}');
-  //       _errorMessage = 'Failed to load data: ${response.statusCode}';
-  //     }
-  //   } catch (e) {
-  //     debugPrint('Error fetching safety check details: $e');
-  //     _errorMessage = 'Error: $e';
-  //   } finally {
-  //     _isLoading = false;
-  //     notifyListeners();
-  //   }
-  // }
 }
